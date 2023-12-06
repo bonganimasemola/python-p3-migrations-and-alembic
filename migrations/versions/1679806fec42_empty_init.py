7f552b4d301114405d6744943cfb070577397e9f
bonganimasemola <bonganimasemola@icloud.com>
2023-12-06 14:27:40 +0300

    migrations and alembic code-along complete

---
+++ migrations/versions/1679806fec42_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 1679806fec42
+Revises: b8cd42c414c8
+Create Date: 2023-12-06 14:15:36.338849
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '1679806fec42'
+down_revision = 'b8cd42c414c8'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
---
+++ migrations/versions/b8cd42c414c8_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: b8cd42c414c8
+Revises: 
+Create Date: 2023-12-06 13:43:35.847782
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'b8cd42c414c8'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
